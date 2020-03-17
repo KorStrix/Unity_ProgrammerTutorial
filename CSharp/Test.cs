6b508f9e5c4a2735637bad066609d6a9afcf3e2e
Strix <KorStrix@users.noreply.github.com>
2020-03-17 16:40:16 +0900

    no message

---
+++ CSharp/Test.cs
@@ -0,0 +1,25 @@
+#region Header
+/*	============================================
+ *	Aurthor 			  : Strix
+ *	Initial Creation Date : 2020-03-17 오후 4:39:20
+ *	Summary 			  : 
+ *  Template 		      : Visual Studio ItemTemplate For Unity V7
+   ============================================ */
+#endregion Header
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+/// <summary>
+/// 
+/// </summary>
+public class Test
+{
+    [Test]
+    public void TestTest()
+    {
+        Debug.Log("TestTest Called");
+    }
+}
